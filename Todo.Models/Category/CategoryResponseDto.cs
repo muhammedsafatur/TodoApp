925d457c703a29af6e4d6eeea66c6942ed9bbec1
muhammedsafatur <msafatur@outlook.com>
2024-11-01 18:24:28 +0300

    Category Dtos Created

---
+++ Todo.Models/Category/CategoryResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Models.Category;
+
+public record CategoryResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+
+};
